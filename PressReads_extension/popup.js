9afd4baff4923c8a7db826e114bccd3b6a780f95
Anusha-Panchumarthi <anusha.panchumarthi@gmail.com>
2022-08-18 18:23:31 +0530

    hell yeah

--- PressReads_extension/popup.js
+++ PressReads_extension/popup.js
@@ -17,10 +17,11 @@
         document.body.append(result)
     }*/
 
-
+    let result;
+  
     document.getElementById("btn").onclick = async () => {
         const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-        let result;
+       
         try {
           [{result}] = await chrome.scripting.executeScript({
             target: {tabId: tab.id},
@@ -29,5 +30,29 @@
         } catch (e) {
           return; 
         }
-        console.log(result)
+       
+        const data = {
+          "text" : result
+      }
+      const options = {
+          method: 'POST',
+          headers:{
+              'Access-Control-Allow-Origin': '*',
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+      }
+      
+      
+      fetch('https://sih-hackathon-api.herokuapp.com/bow_summarise_text/', options)
+      .then((response) => {
+          response.json().then(function(json){
+            document.getElementById('para').innerHTML = JSON.stringify(json.summary);  
+          })
+       //  console.log(res.summary_from_url)
+      })
+      .catch((error) => console.log(error))
+      
       };
+      
+        
